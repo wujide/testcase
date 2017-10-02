1094662284bbdeee16f8d1841879ec9e87cee70e
wujide <wujide@zillionfortune.com>
2017-10-02 10:41:45 +0800

    optimizing

--- test_case/cache_fresh.py
+++ test_case/cache_fresh.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 # __author__='wujide'
 from logging import exception
+
 import paramiko
 from test_case.interface_test_class import InterfaceTest
-from tools.log_read import log_read
 
 
 # read the log from the server; output: str
-def server_log_get(cmd):
-    para_path = r"../info/server"
+def cache_refresh():
+    para_path = r"../info/redis"
     obj = InterfaceTest(para_path)
     d = obj.data_get()
     result = ""
@@ -17,14 +17,24 @@ def server_log_get(cmd):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         print '''------connecting to %s -------- ''' % d['ip']
         ssh.connect(d['ip'], d['port'], d['user'], d['pwd'])
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        result = stdout.read()
+        cmd1 = 'redis-cli'
+        cmd2 = 'auth 123456'
+        cmd3 = 'get user_m_uid1360000929'
+        cmd = r'''redis-cli
+                auth 123456
+                get user_m_uid1360000929
+                '''
+
+        stdin1, stdout1, stderr1 = ssh.exec_command(cmd1)
+        stdin2, stdout2, stderr2 = ssh.exec_command(cmd2)
+        stdin3, stdout3, stderr3 = ssh.exec_command(cmd3)
+        #stdin, stdout, stderr = ssh.exec_command(cmd)
+        result = stdout2.read()
         print "result:", result
         ssh.close()
-    except exception, e:
+    except Exception, e:
         print e
     return result
 
 if __name__ == '__main__':
-    cmd = r'cat /ywdata/tomcat7-appInterface/logs/catalina.out |tail -n 1'
-    server_log_get(cmd)
+    cache_refresh()
