253b3377b1a68375dffdf2b1bdd4da5878ada0fa
wujide <wujide2015@126.com>
2017-10-23 23:18:45 +0800

    modify

--- demo/excel_demo.py
+++ demo/excel_demo.py
@@ -14,16 +14,16 @@ def open_excel(file):
 
 
 # 根据索引获取Excel表格中的数据   colnameindex：表头列名所在行  ，by_index：表的索引
-def excel_table_byindex(file, colnameindex=0, by_index=1):
+def excel_table_byindex(file, colnameindex=0, by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows  # 行数
     ncols = table.ncols  # 列数
-    row_val = table.row_values(colnameindex)  # 某一行数据
+    row_val = table.row_values(colnameindex)  # 第一行的数据，len(row_val) 也即知道了列数
     list = []
     for rownum in range(0, nrows):
         row = table.row_values(rownum)
-        print row
+        # print row
         if row:
             app = {}
             for i in range(len(row_val)):
@@ -39,14 +39,30 @@ def read_excel(file, row_start=0, col_index=0, sheets_index=1):
     nrows = table.nrows  # 行数
     ncols = table.ncols  # 列数
     for row in range(row_start, nrows):
-        row = table.row_values(row)
-        print row
+        row_val = table.row_values(row)
+        print row_val
+
+
+# write data to the file
+def write_excel(file):
+    pass
+
+
+# update the data to the file
+def update_excel(file, row_index, col_index, data):
+    pass
+
+
+# check the data to the file
+def check_excel(file, row_index, col_index):
+    pass
+
 
 if __name__ == "__main__":
     file = "excel_demo.xlsx"
-    read_excel(file, 0, )
+    read_excel(file, 90)
 #    for i in excel_table_byindex(file):
-#       print i
+#        print i
 
 
 '''
