7c836967109dccf64640a39a6e69ab7bb9ad5bd8
wujide <wujide2015@126.com>
2017-10-24 22:08:47 +0800

    add xlwt case

--- demo/excel_demo.py
+++ demo/excel_demo.py
@@ -2,6 +2,8 @@
 # __author__='Administrator'
 
 import xlrd
+import xlwt
+from xlutils.copy import copy
 
 
 # open the excel file
@@ -18,7 +20,6 @@ def excel_table_byindex(file, colnameindex=0, by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows  # 行数
-    ncols = table.ncols  # 列数
     row_val = table.row_values(colnameindex)  # 第一行的数据，len(row_val) 也即知道了列数
     list = []
     for rownum in range(0, nrows):
@@ -33,19 +34,40 @@ def excel_table_byindex(file, colnameindex=0, by_index=0):
 
 
 # read the file
-def read_excel(file, row_start=0, col_index=0, sheets_index=1):
+def read_excel(file, row_start=0, sheets_index=0):
     data = open_excel(file)
     table = data.sheets()[sheets_index]
     nrows = table.nrows  # 行数
-    ncols = table.ncols  # 列数
     for row in range(row_start, nrows):
         row_val = table.row_values(row)
         print row_val
 
 
 # write data to the file
-def write_excel(file):
-    pass
+def write_excel():
+    wbk = xlwt.Workbook()  # 创建工作簿
+    sheet1 = wbk.add_sheet('sheet1')  # 创建sheet
+    row0 = [u'业务', u'状态', u'北京', u'上海', u'广州', u'深圳', u'状态小计', u'合计']
+    column0 = [u'机票', u'船票', u'火车票', u'汽车票', u'其它']
+    status = [u'预订', u'出票', u'退票', u'业务小计']
+    # 生成第一行
+    for i in range(0, len(row0)):
+        sheet1.write(0, i, row0[i])
+    # 生成第一列和最后一列(合并4行)
+    i, j = 1, 0
+    while i < 4 * len(column0) and j < len(column0):
+        sheet1.write_merge(i, i + len(status)-1, 0, 0, column0[j])  # 第一列
+        sheet1.write_merge(i, i + len(status)-1, len(row0)-1, len(row0)-1)  # 最后一列"合计"
+        i += len(status)
+        j += 1
+    sheet1.write_merge(4 * len(column0)+1, 4 * len(column0)+1, 0, 1, u'合计')
+    # 生成第二列
+    col_2 = 0
+    while col_2 < 4 * len(column0):
+        for j in range(0, len(status)):
+            sheet1.write(j + col_2 + 1, 1, status[j])
+        col_2 += len(status)
+    wbk.save('write_excel.xls')  # 保存文件
 
 
 # update the data to the file
@@ -60,7 +82,8 @@ def check_excel(file, row_index, col_index):
 
 if __name__ == "__main__":
     file = "excel_demo.xlsx"
-    read_excel(file, 90)
+    # read_excel(file, 90)
+    write_excel()
 #    for i in excel_table_byindex(file):
 #        print i
 
