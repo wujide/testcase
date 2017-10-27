4ef59dd84f9404d395790b3b98fbcaa529be5318
wujide <wujide@zillionfortune.com>
2017-10-27 16:35:02 +0800

    modify demo

--- demo/threading_demo.py
+++ demo/threading_demo.py
@@ -2,28 +2,29 @@
 # __author__='wujide'
 
 import threading
-from time import ctime,sleep
+from time import ctime, sleep
 
 
 def music(func):
     for i in range(2):
-        print "I was listening to %s. %s" %(func,ctime())
-        sleep(1)
+        print "I was listening to %s. %s" % (func, ctime())
+        #sleep(1)
+
 
 def move(func):
     for i in range(2):
-        print "I was at the %s! %s" %(func,ctime())
-        sleep(5)
+        print "I was at the %s! %s" % (func, ctime())
+        #sleep(5)
 
 threads = []
-t1 = threading.Thread(target=music,args=(u'爱情买卖',))
+t1 = threading.Thread(target=music, args=(u'爱情买卖',))
 threads.append(t1)
-t2 = threading.Thread(target=move,args=(u'阿凡达',))
+t2 = threading.Thread(target=move, args=(u'阿凡达',))
 threads.append(t2)
 
 if __name__ == '__main__':
     for t in threads:
         t.setDaemon(True)
         t.start()
-
-    print "all over %s" %ctime()
+    t.join()
+    print "all over %s" % ctime()
