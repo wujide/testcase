1260457b83d7f60302c7b36293dccb85ea00630d
wujide <wujide2015@126.com>
2017-11-05 23:45:51 +0800

    demo

--- demo/multiprocessing_demo.py
+++ demo/multiprocessing_demo.py
@@ -7,19 +7,24 @@ import os, time, random
 def long_time_task(name):
     print 'Run task %s (%s)...' % (name, os.getpid())
     start = time.time()
-    # time.sleep(random.random() * 3)
+    sum = 0
+    for num in range(1000):
+        sum += num
     end = time.time()
     print 'Task %s runs %0.2f seconds.' % (name, (end - start))
 
 
 if __name__ == '__main__':
+    print "start at :", time.ctime()
     print 'Parent process %s.' % os.getpid()
-    p = Pool()
-    for i in range(10):
-        p.apply_async(long_time_task, args=(i,))
-    print 'Waiting for all subprocesses done...'
     start = time.time()
-    print "start:", time.ctime()
+    p = Pool()
+    print "Pool start at :", time.ctime()
+    for i in range(100):
+        p.apply_async(long_time_task, args=(i,))
+    #print 'Waiting for all subprocesses done...'
+    print "after for:", time.ctime()
+    # print "start:", time.ctime()
     p.close()
     p.join()
     print 'All subprocesses done.'
