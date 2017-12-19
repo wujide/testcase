7e6ce31f61915c580f884174f8362fcce5022312
wujide <wujide@zillionfortune.com>
2017-12-19 15:06:41 +0800

    modidy

--- demo/py_test_demo_2.py
+++ demo/py_test_demo_2.py
@@ -1,17 +1,17 @@
 # coding=utf-8
 # __author__='wujide'
-print [round(pow(1.07, y), 2) for y in range(2, 11)]
-print [round(pow(1.07, y), 2) for y in range(2, 21)]
-print [round(pow(1.07, y), 2) for y in range(2, 31)]
+print [round(pow(1.07, y), 2) for y in range(1, 11)]
+print [round(pow(1.07, y), 2) for y in range(1, 21)]
+print [round(pow(1.07, y), 2) for y in range(1, 31)]
 print "-----------------------------------"
-print [round(pow(1.10, y), 2) for y in range(2, 11)]
-print [round(pow(1.10, y), 2) for y in range(2, 21)]
-print [round(pow(1.10, y), 2) for y in range(2, 31)]
+print [round(pow(1.10, y), 2) for y in range(1, 11)]
+print [round(pow(1.10, y), 2) for y in range(1, 21)]
+print [round(pow(1.10, y), 2) for y in range(1, 31)]
 print "-----------------------------------"
-print [round(pow(1.15, y), 2) for y in range(2, 11)]
-print [round(pow(1.15, y), 2) for y in range(2, 21)]
-print [round(pow(1.15, y), 2) for y in range(2, 31)]
+print [round(pow(1.15, y), 2) for y in range(1, 11)]
+print [round(pow(1.15, y), 2) for y in range(1, 21)]
+print [round(pow(1.15, y), 2) for y in range(1, 31)]
 print "-----------------------------------"
-print [round(pow(1.20, y), 2) for y in range(2, 11)]
-print [round(pow(1.20, y), 2) for y in range(2, 21)]
-print [round(pow(1.20, y), 2) for y in range(2, 31)]
+print [round(pow(1.20, y), 2) for y in range(1, 11)]
+print [round(pow(1.20, y), 2) for y in range(1, 21)]
+print [round(pow(1.20, y), 2) for y in range(1, 31)]
