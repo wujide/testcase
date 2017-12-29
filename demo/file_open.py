a7d593a8e62cdd5639bb8228b483ab3d2fbafc6b
wujide <wujide@zillionfortune.com>
2017-12-29 17:49:47 +0800

    modify

--- demo/file_open.py
+++ demo/file_open.py
@@ -1,14 +1,8 @@
 # coding=utf-8
 # __author__='Administrator'
 
+import fileinput
 
 
-
-
-with open(r'../info/url', 'r') as f_url:
-    lines = f_url.readline()
-    print lines
-    lines_strip = lines.split('=')
-    url = lines_strip[1]
-    print url
-
+for line in fileinput.input(r'test_file'):
+    print line
