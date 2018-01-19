954baf85c519e59c0cf060d326e35e0d28a33e62
wujide <wujide@zillionfortune.com>
2018-01-19 09:56:46 +0800

    easy to be understood

--- demo/queen.py
+++ demo/queen.py
@@ -4,11 +4,11 @@
 import random
 
 
-def conflict(state, col):
+def conflict(state, nextX):
     # row为行，col为列, state为皇后的状态
-    row = len(state)    # 目前没冲突的行数
-    for i in range(row):    # 从1- row行依次检测是否与row+1行皇后冲突
-        if abs(state[i] - col) in (0, row - i):
+    nextY = len(state)    # 目前没冲突的行数
+    for i in range(nextY):    # 从1- row行依次检测是否与row+1行皇后冲突
+        if abs(state[i] - nextX) in (0, nextY - i):
             return True
     return False
 
@@ -17,9 +17,9 @@ def queens(num=8, state=()):
     # 生成器函数
     for pos in range(num):
         if not conflict(state, pos):
-            if len(state) == num - 1:
+            if len(state) == num - 1:  # 产生当前皇后的位置信息
                 yield (pos,)
-            else:
+            else:  # 把当前皇后的位置信息，添加到状态列表里，并传递给下一皇后。
                 for result in queens(num, state + (pos,)):
                     yield (pos,) + result
 
