87a769b8bcc0a4b81bcfd314dff799bbc90682ac
wujide <wujide2015@126.com>
2018-07-01 23:16:12 +0800

    add unittest demo

---
+++ unittest_demo/mathfunc.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# __author__='wujide'
+
+
+def add(a, b):
+    return a+b
+
+
+def minus(a, b):
+    return a-b
+
+
+def multi(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
---
+++ unittest_demo/test_mathfunc.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+# __author__='wujide'
+
+# note: https://blog.csdn.net/huilan_same/article/details/52944782
+import unittest
+from mathfunc import *
+
+
+class TestMathFunc(unittest.TestCase):
+    """Test mathfuc.py"""
+
+    def setUp(self):
+        print "do something before test.Prepare environment."
+
+    def tearDown(self):
+        print "do something after test.Clean up."
+
+    # 如果想要在所有case执行之前准备一次环境，并在所有case执行结束之后再清理环境，
+    # 我们可以用 setUpClass() 与 tearDownClass():
+
+    @classmethod
+    def setUpClass(cls):
+        print "This setUpClass() method only called once."
+
+    @classmethod
+    def tearDownClass(cls):
+        print "This tearDownClass() method only called once too."
+
+    def test_add(self):
+        """Test method add(a, b)"""
+        self.assertEqual(3, add(1, 2))
+        self.assertNotEqual(3, add(2, 2))
+
+    def test_minus(self):
+        """Test method minus(a, b)"""
+        self.assertEqual(1, minus(3, 2))
+
+    def test_multi(self):
+        """Test method multi(a, b)"""
+        self.assertEqual(6, multi(2, 3))
+
+    # skip装饰器
+    @unittest.skip("I don't want to run this case.")
+    def test_divide(self):
+        """Test method divide(a, b)"""
+        self.assertEqual(2, divide(6, 3))
+        self.assertEqual(2.5, divide(5, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
